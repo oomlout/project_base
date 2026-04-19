75ac642374f9c691911fda56d2b147b89b4d19f6
Oom Lout <aaron@oomlout.com>
2026-04-19 19:18:56 +0100

    working

--- scad.py
+++ scad.py
@@ -40,12 +40,13 @@ def get_parts(kwargs, project_name, oomp_mode):
     parts = []
     part_default = scad_help.get_default_part(project_name)
 
-    part = copy.deepcopy(part_default)
+    
     if True:        
+        part = copy.deepcopy(part_default)
         p3 = copy.deepcopy(kwargs)
         p3["width"] = 2
         p3["height"] = 3
-        p3["thickness"] = 30
+        p3["thickness"] = 90
         #p3["extra"] = ""
         part["kwargs"] = p3
         nam = "drawer_basic"
@@ -57,6 +58,7 @@ def get_parts(kwargs, project_name, oomp_mode):
 
     #holder
     if True:        
+        part = copy.deepcopy(part_default)
         heights = [5,3]
         for height in heights:
             p3 = copy.deepcopy(kwargs)
@@ -71,6 +73,21 @@ def get_parts(kwargs, project_name, oomp_mode):
                 p3["oomp_size"] = nam
             parts.append(part)
 
+
+    #front
+    if True:
+        part = copy.deepcopy(part_default)
+        p3 = copy.deepcopy(kwargs)
+        p3["width"] = 2
+        p3["height"] = 90
+        p3["thickness"] = 2
+        #p3["extra"] = ""
+        part["kwargs"] = p3
+        nam = "drawer_basic_front"
+        part["name"] = nam
+        if oomp_mode == "oobb":
+            p3["oomp_size"] = nam
+        parts.append(part)
 
     return parts
 
@@ -202,6 +219,154 @@ def get_drawer_basic(thing, **kwargs):
         pos12[0] -= (width_mm - thickness_wall) / 2
         poss.append(pos12)
         p3["pos"] = poss
+        oobb.append_full(thing,**p3)
+
+    #add screw holes in corner
+    if True:
+        thickness_front = 2
+        oobb_width = int(width_mm / 15)
+        oobb_height = int(height_mm / 15)
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_screw_countersunk"
+        dep_screw = 15
+        p3["depth"] = dep_screw
+        p3["radius_name"] = "m3"
+        pos1 = copy.deepcopy(pos)
+        pos1[1] -= height_mm / 2 + thickness_wall
+        pos1[2] += depth / 2
+        poss = []
+        pos11 = copy.deepcopy(pos1)
+        pos11[0] += (width_mm - 15) / 2
+        pos11[2] += (depth_mm - 15) / 2
+        poss.append(pos11)
+        pos12 = copy.deepcopy(pos1)
+        pos12[0] -= (width_mm - 15) / 2
+        pos12[2] += (depth_mm - 15) / 2
+        poss.append(pos12)
+        pos13 = copy.deepcopy(pos1)
+        pos13[0] += (width_mm - 15) / 2
+        pos13[2] -= (depth_mm - 15) / 2
+        poss.append(pos13)
+        pos14 = copy.deepcopy(pos1)
+        pos14[0] -= (width_mm - 15) / 2
+        pos14[2] -= (depth_mm - 15) / 2
+        poss.append(pos14)
+        p3["rot"] = [90, 0, 0]
+        p3["pos"] = poss
+        p3["m"] = "#"
+        oobb.append_full(thing,**p3)
+
+
+    if prepare_print:
+        scad_help.prepare_base_for_print(thing, pos, **kwargs)
+
+def get_drawer_basic_front(thing, **kwargs):
+
+    prepare_print = kwargs.get("prepare_print", False)
+    width = kwargs.get("width", 1)
+    height = kwargs.get("height", 1)
+    depth = kwargs.get("thickness", 3)                    
+    rot = kwargs.get("rot", [0, 0, 0])
+    pos = kwargs.get("pos", [0, 0, 0])
+    extra = kwargs.get("extra", "")
+    
+    if False:
+        #add plate
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "positive"
+        p3["shape"] = f"oobb_plate"    
+        p3["depth"] = depth
+        #p3["holes"] = True         uncomment to include default holes
+        #p3["m"] = "#"
+        pos1 = copy.deepcopy(pos)         
+        p3["pos"] = pos1
+        oobb.append_full(thing,**p3)
+        
+        #add holes seperate
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "p"
+        p3["shape"] = f"oobb_holes"
+        p3["both_holes"] = True  
+        p3["depth"] = depth
+        p3["holes"] = "perimeter"
+        #p3["m"] = "#"
+        pos1 = copy.deepcopy(pos)         
+        p3["pos"] = pos1
+        oobb.append_full(thing,**p3)
+
+    #add the cubes
+    width_mm = width * 42 - 3
+    height_mm = height
+    depth_mm = depth
+    
+    if True:
+        #base
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "positive"
+        p3["shape"] = f"oobb_cube"
+        p3["size"] = [width_mm, height_mm, depth]
+        pos1 = copy.deepcopy(pos)         
+        p3["pos"] = pos1
+        oobb.append_full(thing,**p3)
+        
+
+    #add screw holes in corners
+    if True:
+        oobb_width = int(width_mm / 15)
+        oobb_height = int(height_mm / 15)
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_screw_countersunk"
+        p3["depth"] = 20
+        p3["radius_name"] = "m3"
+        pos1 = copy.deepcopy(pos)
+        pos1[2] += depth
+        poss = []
+        pos11 = copy.deepcopy(pos1)
+        pos11[0] += (width_mm - 15) / 2
+        pos11[1] += (height_mm - 15) / 2
+        poss.append(pos11)
+        pos12 = copy.deepcopy(pos1)
+        pos12[0] -= (width_mm - 15) / 2
+        pos12[1] += (height_mm - 15) / 2
+        poss.append(pos12)
+        pos13 = copy.deepcopy(pos1)
+        pos13[0] += (width_mm - 15) / 2
+        pos13[1] -= (height_mm - 15) / 2
+        poss.append(pos13)
+        pos14 = copy.deepcopy(pos1)
+        pos14[0] -= (width_mm - 15) / 2
+        pos14[1] -= (height_mm - 15) / 2
+        poss.append(pos14)
+        p3["pos"] = poss
+        p3["m"] = "#"
+        oobb.append_full(thing,**p3)
+
+
+    #label_size
+    label_width = 3*25.4
+    label_height = 2*25.4
+    label_depth = depth - 0.5
+    border_width = 10
+    clearance = 1
+
+    if True:
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "negative"
+        p3["shape"] = f"oobb_cube"
+        p3["size"] = [label_width - border_width/2, label_height - border_width/2, depth]
+        pos1 = copy.deepcopy(pos)         
+        pos1[2] += 0
+        p3["pos"] = pos1
+        oobb.append_full(thing,**p3)
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "negative"
+        p3["shape"] = f"oobb_cube"
+        p3["size"] = [label_width+clearance, label_height+clearance, label_depth]
+        pos1 = copy.deepcopy(pos)
+        pos1[2] += 0
+        p3["pos"] = pos1
         oobb.append_full(thing,**p3)
 
 
