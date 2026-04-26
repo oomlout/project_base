11b859fa708ba31abdb1e27b0536db5573549d13
Oom Lout <aaron@oomlout.com>
2026-04-26 15:36:00 +0100

    fixed up

--- working_oomp.py
+++ working_oomp.py
@@ -67,7 +67,7 @@ def create_generic(**kwargs):
         #icon
         if True:
             count += 1     
-            icon_detail = "please include the thread size and length in the image as text, add two extra icons in the back, one that ryhmes with the diameter and another that rhymes with the length. Put some time into searching to find the rhymes"
+            icon_detail = "please include the thread size and length in the image as text, add extra icons in the back, one that ryhmes with the diameter and one or two that rhymes with the length, if the length is less than 14 just one if it is greater do two, one rhyming for each part of the number ie for twenty five one that rhymes with twenty and one that rhymes with five. Put some time into searching to find the rhymes"
             oomp_helper.add_icon(part=part, count=count, mode_ai_wait=mode_ai_wait, icon_detail=icon_detail)
 
         #folder_project = "helen_personal_chart_bribe_bank"
