2ee32fdce9cf45c6fc98e125cbf7389da1ab67e4
Oom Lout <aaron@oomlout.com>
2026-05-05 13:44:20 +0100

    added extra runs

---
+++ action_make_scad.py
@@ -0,0 +1,21 @@
+import copy
+import working_scad
+
+def main(**kwargs):
+    kwargs["run_oomp_populate"] = True
+
+    kwargs["run_oomp"] = True
+
+    kwargs["run_action"] = False
+    #kwargs["run_action"] = False
+
+    kwargs["run_scad"] = True
+
+    kwargs["generate_stl"] = True
+
+    import working
+    working.run(**kwargs)
+
+if __name__ == '__main__':
+    kwargs = {}
+    main(**kwargs)
